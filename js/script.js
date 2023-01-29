128dd09b5b62902a9bb40d590e3217c96625e800
unknown <emilstrelis@inbox.lv>
2023-01-29 20:14:35 +0200

    updated design

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*Card on click function*/
 var cards = document.querySelectorAll('.bakery_card');
 
 [...cards].forEach((bakery_card)=>{
@@ -5,3 +6,25 @@ var cards = document.querySelectorAll('.bakery_card');
         bakery_card.classList.toggle('is-flipped');
   });
 });
+
+/*Login change div function*/
+function pieslegties() { 
+	document.getElementById("registration").style.display="none"; 
+	document.getElementById("signin").style.display="block"; 
+}
+
+function registreties() { 
+	document.getElementById("registration").style.display="block";
+    document.getElementById("new_password").style.display="none"; 
+	document.getElementById("signin").style.display="none"; 
+}
+
+function atjaunot_paroli() { 
+	document.getElementById("signin").style.display="none"; 
+	document.getElementById("change_password").style.display="block"; 
+}
+
+function mainit_paroli() { 
+	document.getElementById("change_password").style.display="none"; 
+	document.getElementById("new_password").style.display="block"; 
+}
